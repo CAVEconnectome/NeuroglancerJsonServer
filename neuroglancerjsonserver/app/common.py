c7174d3cd9f5fbfa8f19b21b896d278d33829013
Forrest Collman <forrest.collman@gmail.com>
2024-04-05 10:10:58 -0700

    adding git tag

--- neuroglancerjsonserver/app/common.py
+++ neuroglancerjsonserver/app/common.py
@@ -5,21 +5,37 @@ import time
 
 from neuroglancerjsonserver.app import app_utils
 from neuroglancerjsonserver import __version__
+import subprocess
 
 
 __api_versions__ = [0]
 
+
 # -------------------------------
 # ------ Access control and index
 # -------------------------------
+def get_git_tag():
+    try:
+        __tag = (
+            subprocess.check_output(["git", "describe", "--tags"])
+            .strip()
+            .decode("utf-8")
+        )
+        return tag
+    except subprocess.CalledProcessError:
+        return "None"
+
+
+__tag__ = get_git_tag()
+
 
 def index():
-    return f"NeuroglancerJsonServer - v{__version__}"
+    return f"NeuroglancerJsonServer - v{__version__} - tag {__tag__} "
 
 
 def home():
     resp = make_response()
-    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers["Access-Control-Allow-Origin"] = "*"
     acah = "Origin, X-Requested-With, Content-Type, Accept"
     resp.headers["Access-Control-Allow-Headers"] = acah
     resp.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
@@ -31,6 +47,7 @@ def home():
 # ------ Measurements and Logging
 # -------------------------------
 
+
 def before_request():
     g.request_start_time = time.time()
 
@@ -39,10 +56,16 @@ def after_request(response):
     dt = (time.time() - g.request_start_time) * 1000
 
     url_split = request.url.split("/")
-    current_app.logger.info("%s - %s - %s - %s - %f.3" %
-                            (request.path.split("/")[-1], "1",
-                             "".join([url_split[-2], "/", url_split[-1]]),
-                             str(request.data), dt))
+    current_app.logger.info(
+        "%s - %s - %s - %s - %f.3"
+        % (
+            request.path.split("/")[-1],
+            "1",
+            "".join([url_split[-2], "/", url_split[-1]]),
+            str(request.data),
+            dt,
+        )
+    )
 
     print("Response time: %.3fms" % (dt))
     return response
@@ -52,11 +75,16 @@ def internal_server_error(error):
     dt = (time.time() - g.request_start_time) * 1000
 
     url_split = request.url.split("/")
-    current_app.logger.error("%s - %s - %s - %s - %f.3" %
-                             (request.path.split("/")[-1],
-                              "Server Error: " + error,
-                              "".join([url_split[-2], "/", url_split[-1]]),
-                              str(request.data), dt))
+    current_app.logger.error(
+        "%s - %s - %s - %s - %f.3"
+        % (
+            request.path.split("/")[-1],
+            "Server Error: " + error,
+            "".join([url_split[-2], "/", url_split[-1]]),
+            str(request.data),
+            dt,
+        )
+    )
     return 500
 
 
@@ -64,16 +92,23 @@ def unhandled_exception(e):
     dt = (time.time() - g.request_start_time) * 1000
 
     url_split = request.url.split("/")
-    current_app.logger.error("%s - %s - %s - %s - %f.3" %
-                             (request.path.split("/")[-1],
-                              "Exception: " + str(e),
-                              "".join([url_split[-2], "/", url_split[-1]]),
-                              str(request.data), dt))
+    current_app.logger.error(
+        "%s - %s - %s - %s - %f.3"
+        % (
+            request.path.split("/")[-1],
+            "Exception: " + str(e),
+            "".join([url_split[-2], "/", url_split[-1]]),
+            str(request.data),
+            dt,
+        )
+    )
     return 500
+
 
 # -------------------
 # ------ Applications
 # -------------------
+
 
 def get_json(json_id):
     db = app_utils.get_json_db()
@@ -101,11 +136,13 @@ def add_json(json_id=None, timestamp=None):
     except ValueError:
         raise ValueError
 
-    json_id = db.add_json(request.data, user_id=user_id, 
-                          json_id=json_id, date=timestamp)
+    json_id = db.add_json(
+        request.data, user_id=user_id, json_id=json_id, date=timestamp
+    )
     url_base = request.url.strip("/").rsplit("/", 1)[0]
 
     return jsonify("{}/{}".format(url_base, json_id))
+
 
 def get_properties(json_id):
     db = app_utils.get_property_db()
@@ -114,12 +151,14 @@ def get_properties(json_id):
 
     return jsonify(json.loads(json_data))
 
+
 def get_raw_properties(json_id):
     db = app_utils.get_property_db()
 
     json_data = db.get_json(int(json_id), decompress=False)
 
     return json_data
+
 
 def add_properties(json_id=None, timestamp=None):
     user_id = str(g.auth_user["id"])
@@ -131,11 +170,13 @@ def add_properties(json_id=None, timestamp=None):
     except ValueError:
         raise ValueError
 
-    json_id = db.add_json(request.data, user_id=user_id, 
-                          json_id=json_id, date=timestamp)
+    json_id = db.add_json(
+        request.data, user_id=user_id, json_id=json_id, date=timestamp
+    )
     url_base = request.url.strip("/").rsplit("/", 1)[0]
 
     return jsonify("{}/{}".format(url_base, json_id))
+
 
 def table_info():
     user_id = str(g.auth_user["id"])
