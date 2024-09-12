b2e41ea8a63ca4c5ff67f6c460f1ef670adfa25b
Ben Pedigo <benjamindpedigo@gmail.com>
2024-09-12 10:26:14 -0700

    initial logic

--- neuroglancerjsonserver/backend/database.py
+++ neuroglancerjsonserver/backend/database.py
@@ -1,8 +1,9 @@
+import datetime
 import os
 import zlib
-import datetime
-from google.cloud import datastore
+
 from datastoreflex import DatastoreFlex
+from google.cloud import datastore
 
 HOME = os.path.expanduser("~")
 
@@ -14,16 +15,21 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = (
 
 class JsonDataBase(object):
     def __init__(
-        self, table_name, project_id=None, client=None, credentials=None, column="v1"
+        self,
+        table_name,
+        project_id=None,
+        client=None,
+        credentials=None,
+        columns=("v2", "v1"),
     ):
         if client is not None:
             self._client = client
         else:
             assert project_id is not None
-            self._client = DatastoreFlex(project=project_id,  credentials=credentials)
+            self._client = DatastoreFlex(project=project_id, credentials=credentials)
 
         self._namespace = table_name
-        self._column = column
+        self._columns = columns
 
     @property
     def client(self):
@@ -42,8 +48,9 @@ class JsonDataBase(object):
         return "ngl_json"
 
     @property
-    def json_column(self):
-        return self._column
+    def json_columns(self) -> tuple:
+        """Tuple of column names in decreasing precedence for where JSONs are stored."""
+        return self._columns
 
     def add_json(self, json_data, user_id, json_id=None, date=None):
         if json_id is None:
@@ -59,13 +66,16 @@ class JsonDataBase(object):
         if entity is not None:
             raise Exception(f"[{self.namespace}][{key}][{json_id}] ID already exists.")
 
-        entity = datastore.Entity(key, exclude_from_indexes=(self.json_column,))
+        entity = datastore.Entity(key, exclude_from_indexes=self.json_columns)
 
-        entity[self.json_column] = zlib.compress(json_data)
+        # always put into the first column for new data
+        # could play with different schemes here in the future
+        entity[self.columns[0]] = zlib.compress(json_data)
+        
         entity["access_counter"] = int(1)
         entity["user_id"] = user_id
 
-        now = datetime.datetime.utcnow()
+        now = datetime.datetime.now(datetime.UTC)
         if date is None:
             date = now
 
@@ -81,9 +91,18 @@ class JsonDataBase(object):
 
         entity = self.client.get(key)
 
-        assert self.json_column in entity.keys()
+        # look for the JSON data in decreasing order of precedence
+        found_column = None
+        for column in self.json_columns:
+            if column in entity.keys():
+                found_column = column
+                break
+        if found_column is None:
+            raise ValueError(
+                f"[{self.namespace}][{key}][{json_id}] No JSON data found."
+            )
 
-        json_data = entity.get(self.json_column)
+        json_data = entity.get(found_column)
 
         if decompress:
             json_data = zlib.decompress(json_data)
@@ -93,7 +112,7 @@ class JsonDataBase(object):
         else:
             entity["access_counter"] = int(2)
 
-        entity["date_last"] = datetime.datetime.utcnow()
+        entity["date_last"] = datetime.datetime.now(datetime.UTC)
 
         self.client.put(entity)
 
